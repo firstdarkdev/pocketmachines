17a35281d522402d7a4af55719f047c4b7fb1ac6
hypherionmc <hypherionmc@gmail.com>
2024-11-20 11:44:57 +0200

    [BUG] Fix Anvil voiding items when the GUI is closed

--- 1.21/Common/src/main/java/com/hypherionmc/pocketmachines/common/items/PocketAnvil.java
+++ 1.21/Common/src/main/java/com/hypherionmc/pocketmachines/common/items/PocketAnvil.java
@@ -6,11 +6,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.AnvilMenu;
+import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.AnvilBlock;
 import org.jetbrains.annotations.NotNull;
 
 @NoArgsConstructor
@@ -20,17 +23,13 @@ public class PocketAnvil extends BasePocketGuiItem {
 
     @Override
     public void openMenu(@NotNull Level level, @NotNull Player player, @NotNull InteractionHand hand) {
-        player.openMenu(new MenuProvider() {
+        player.openMenu(new SimpleMenuProvider((containerId, playerInventory, playerEntity) -> new AnvilMenu(containerId, playerInventory, ContainerLevelAccess.create(level, player.blockPosition())) {
             @Override
-            public @NotNull Component getDisplayName() {
-                return TITLE;
+            public boolean stillValid(Player arg) {
+                return true;
             }
+        }, TITLE));
 
-            @Override
-            public @NotNull AbstractContainerMenu createMenu(int containerId, @NotNull Inventory inventory, @NotNull Player player) {
-                return new AnvilMenu(containerId, inventory);
-            }
-        });
         player.awardStat(Stats.INTERACT_WITH_ANVIL);
     }
 
