0e69bc5494541cf71d48333d83a3f63f1d6fdf87
HypherionSA <hypherionmc@gmail.com>
2025-06-21 14:22:57 +0200

    [FEAT] Lit State for Pocket Furnaces

--- Common/src/main/java/com/hypherionmc/pocketmachines/common/items/PocketBlastFurnace.java
+++ Common/src/main/java/com/hypherionmc/pocketmachines/common/items/PocketBlastFurnace.java
@@ -3,14 +3,18 @@ package com.hypherionmc.pocketmachines.common.items;
 import com.hypherionmc.pocketmachines.common.inventory.PocketBlastFurnaceInventory;
 import com.hypherionmc.pocketmachines.common.items.base.BaseTickablePocketItem;
 import com.hypherionmc.pocketmachines.common.world.PersistedMachines;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomModelData;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 import static com.hypherionmc.pocketmachines.ModConstants.makeResourceKey;
 
@@ -28,6 +32,14 @@ public class PocketBlastFurnace extends BaseTickablePocketItem<PocketBlastFurnac
     @Override
     public void tickItem(PocketBlastFurnaceInventory container, @NotNull ItemStack stack, ServerLevel level, @NotNull Entity entity, EquipmentSlot slot) {
         container.tick(level);
+
+        CustomModelData data = stack.getOrDefault(DataComponents.CUSTOM_MODEL_DATA, CustomModelData.EMPTY);
+        String oldLitState = data.getString(0) == null ? "unlit" : data.getString(0);
+        String currentLitState = container.isLit() ? "lit" : "unlit";
+
+        if (!oldLitState.equals(currentLitState)) {
+            stack.set(DataComponents.CUSTOM_MODEL_DATA, new CustomModelData(List.of(), List.of(), List.of(currentLitState), List.of()));
+        }
     }
 
 }
