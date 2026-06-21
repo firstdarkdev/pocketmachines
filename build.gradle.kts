664ffd67f69d77868347db82e77922b868874800
HypherionSA <git@hypherionmc.com>
2026-06-21 22:25:00 +0200

    [CHORE] Convert to new Orion Patcher setup

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import codechicken.diffpatch.util.PatchMode
+
+plugins {
+    id("java")
+    id("com.hypherionmc.modutils.orion.porting") version "2.0.22"
+}
+
+orionporting {
+    upstreamBranch = "dev"
+    // Enable During Porting
+    patchMode = PatchMode.FUZZY
+    porting(
+            "1.21.11",
+            "1.21.9",
+            "1.21.6",
+            "1.21.5",
+            "1.21.4",
+            "1.21.2",
+            "1.21",
+            "1.20.1"
+    )
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://mcentral.firstdark.dev/releases")
+        maven("https://maven.firstdark.dev/releases")
+        maven("https://maven.firstdark.dev/snapshots")
+    }
+}
+
+rootProject.name = "pocketmachines-lts"
